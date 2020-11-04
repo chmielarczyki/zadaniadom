cb614dd9595c18219f6c2ed3256e2fdc0a813851
chmielarczyki <iga.chmielarczyk@gmail.com>
2020-11-04 22:24:02 +0100

    27239

---
+++ js/27239/js/main.js
@@ -0,0 +1,9 @@
+function myFunction(n) {
+    if (100 <= n && n <= 200) {
+        console.log('Liczba znajduje się w przediale');
+    } else {
+        console.log('Liczba nie znajduje się w pzrdziale');
+    }
+}
+
+myFunction(130);
