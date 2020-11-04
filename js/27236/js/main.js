f44b0c35525e1da692136de1c3fff962ad3d1caa
chmielarczyki <iga.chmielarczyk@gmail.com>
2020-11-04 21:30:50 +0100

    27236

---
+++ js/27236/js/main.js
@@ -0,0 +1,12 @@
+let num1 = 2;
+let num2 = 5;
+
+function myFunction(num1, num2) {
+    if (num1 > num2) {
+        console.log(num1);
+    } else {
+        console.log(num2);
+    }
+}
+
+myFunction(num1, num2);
