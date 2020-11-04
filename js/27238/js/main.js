e27a8d249ffffc6e1d28fca7ae77fdfcaa80e317
chmielarczyki <iga.chmielarczyk@gmail.com>
2020-11-04 22:12:31 +0100

    27238

---
+++ js/27238/js/main.js
@@ -0,0 +1,12 @@
+let a = 'Uwielbiam JavaScript';
+let b = 'Jestem świetnym programistą'; 
+
+function myFunction(a, b) {
+    if (a.length > b.length){
+        console.log(a);
+    } else {
+        console.log(b);
+    }
+}
+
+myFunction(a, b);
