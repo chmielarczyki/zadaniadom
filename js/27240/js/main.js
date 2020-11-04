ceaf8531608b8b34b2bce5d2f7c01bc7514c75f9
chmielarczyki <iga.chmielarczyk@gmail.com>
2020-11-04 22:38:53 +0100

    27240

---
+++ js/27240/js/main.js
@@ -0,0 +1,9 @@
+function myFunction(a, b) {
+    if ((a === 20 || b === 20) || (a + b) <= 20) {
+        console.log('true');
+    } else {
+        console.log('false');
+    }
+} 
+
+myFunction(200, 5);
